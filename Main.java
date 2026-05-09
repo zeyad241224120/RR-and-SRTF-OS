0c9d803aea4e738e92bd234b2bd55a8288d51a3b
zeyad <sherefzeyad2412@gmail.com>
2026-05-10 02:06:06 +0300

    Initialize Main class with UI setup

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+
+        SwingUtilities.invokeLater(() -> new SchedulingProjectC2().setVisible(true));
+    }
+}
